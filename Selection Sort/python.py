4dce05ce6288a7676f4b28b2370671e4366d5cf8
EdibleHerald <142949302+EdibleHerald@users.noreply.github.com>
2025-10-20 14:13:18 -0400

    Added Selection Sort folder with Python example, also updated README to reflect java usage

---
+++ Selection Sort/python.py
@@ -0,0 +1,29 @@
+# Selection Sort:
+# 
+#   - Goes through an array and grabs smallest number
+#   - Adds it to new array (sorted smallest to largest)
+#   - Should result in a new, sorted smallest to largest array. 
+#   Big O Notation: O(n^2)
+
+testArray = [2245,621,25,844,7,10,15]
+
+def findSmallest(arr):
+    smallest = arr[0]
+    smallest_index = 0
+
+    for i in range(1,len(arr)):
+        # Compare to entire array
+        if arr[i]<smallest:
+            smallest = arr[i]
+            smallest_index = i
+    
+    return smallest_index
+
+def selectionSort(arr):
+    # Use findSmallest(arr) to get smallest number
+    newArr = []
+    copiedArr = list(arr)
+    for i in range(len(copiedArr)):
+        smallest = findSmallest(copiedArr)
+        newArr.append(copiedArr.pop(smallest))
+    return newArr
